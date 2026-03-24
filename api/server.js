bfb0c19a618f96c13ec1f75d1fa4bccad5cbce11
Júlíus Snær <51999860+juliusSnaerPalsson@users.noreply.github.com>
2026-03-24 18:23:12 +0000

    Fix console log syntax in server.js

--- api/server.js
+++ api/server.js
@@ -50,4 +50,4 @@ res.json({ ok: true, data });
 });
 
 const PORT = process.env.PORT || 4000;
- app.listen(PORT, () => console.log(API listening on ${PORT}));
+app.listen(PORT, () => console.log(API listening on ${PORT}));
